73078daf24250088c95a2b21ec3934df6b6d3731
Shafranskyi <shafranskyi_ak16@nuwm.edu.ua>
2020-02-27 00:26:56 +0200

    Added StudentsController

---
+++ StudentsLearning/Controllers/StudentsController.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using BusinessLogic.Helpers;
+using BusinessLogic.Interfaces;
+using StudentsLearning.Helpers;
+using StudentsLearning.ViewModels.StudentViewModels;
+using StudentsLearning.ViewModels.UserViewModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StudentsLearning.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IStudentService _studentService;
+        private readonly IMapper _mapper;
+
+        public StudentsController(IStudentService studentService, IMapper mapper)
+        {
+            _studentService = studentService;
+            _mapper = mapper;
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetStudentsForAdmin([FromQuery]StudentQueryViewModel studentViewModel)
+        {
+            var queryDTO = _mapper.Map<QueryParamsDTO>(studentViewModel);
+            var result = await _studentService.GetStudents(queryDTO);
+            var studentsList = _mapper.Map<IEnumerable<UserForAdminViewModel>>(result.List);
+            var pageInfo = result.Info;
+            Response.AddPagination(pageInfo.CurrentPage, pageInfo.PageSize, pageInfo.TotalCount, pageInfo.TotalPages);
+            return Ok(studentsList);
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpGet("[action]/{id}")]
+        public async Task<IActionResult> GetStudent(int id)
+        {
+            var student = await _studentService.GetStudent(id);
+            if (student == null)
+                return BadRequest("User was not found");
+            return Ok(_mapper.Map<UserWithCoursesViewModel>(student));
+        }
+    }
+}
