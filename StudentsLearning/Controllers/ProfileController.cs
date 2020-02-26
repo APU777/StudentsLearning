1901f670aa25bc961455bf389485dbc2a7239375
Shafranskyi <shafranskyi_ak16@nuwm.edu.ua>
2020-02-27 00:25:21 +0200

    Added ProfileController

---
+++ StudentsLearning/Controllers/ProfileController.cs
@@ -0,0 +1,66 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using BusinessLogic.DTO.UserDTO;
+using BusinessLogic.Interfaces;
+using StudentsLearning.ViewModels.UserViewModels;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using StudentsLearning.Helpers;
+using Hangfire;
+
+namespace StudentsLearning.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfileController : ControllerBase
+    {
+        private readonly IProfileService _profile;
+        private readonly IMapper _mapper;
+        private readonly IBackgroundJobClient _backgroundJob;
+        private readonly PhotoSettings _photoSettings;
+
+        public ProfileController(IProfileService profile,
+                                 IMapper mapper,
+                                 IOptions<PhotoSettings> options,
+                                 IBackgroundJobClient backgroundJob)
+        {
+            _profile = profile;
+            _mapper = mapper;
+            _backgroundJob = backgroundJob;
+            _photoSettings = options.Value;
+        }
+
+        [Authorize]
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetProfileInfo()
+        {
+            var user = await _profile.GetProfileInfo(int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value));
+            if (user == null)
+                return BadRequest("Something went wrong");
+            return Ok(_mapper.Map<UserViewModel>(user));
+        }
+
+        [Authorize(Roles = "student")]
+        [HttpPut("{userId}/[action]")]
+        public async Task<IActionResult> UpdateProfileInfo(int userId, [FromForm]UpdateUserProfileViewModel userInfo)
+        {
+            if (userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+                return Unauthorized();
+            if (userInfo.File != null)
+            {
+                if (userInfo.File.Length > _photoSettings.MaxBytes)
+                    return BadRequest("Maximum file size exceeded");
+
+                if (!_photoSettings.IsSupported(userInfo.File.FileName))
+                    return BadRequest("Invalid file type");
+            }
+            var userInfoForUpdate = _mapper.Map<UpdateUserProfileDTO>(userInfo);
+            var user = await _profile.UpdateProfileInfo(int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value), userInfoForUpdate);
+            if (user == null)
+                return BadRequest("Something went wrong");
+            return Ok(_mapper.Map<UserViewModel>(user));
+        }
+    }
+}
