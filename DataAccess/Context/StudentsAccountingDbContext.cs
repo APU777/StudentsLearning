7611bd2912114daa2dcc6f9ab9aa6bb9a82ab12f
Shafranskyi <shafranskyi_ak16@nuwm.edu.ua>
2020-02-27 22:53:06 +0200

    Added context in data project

---
+++ DataAccess/Context/StudentsAccountingDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using DataAccess.Configuration.InitialData;
+using DataAccess.Configuration.RelationConfig;
+using DataAccess.Entities;
+
+namespace DataAccess.Context
+{
+    public class StudentsAccountingDbContext :  IdentityDbContext<User, IdentityRole<int>, int>
+    {
+        public StudentsAccountingDbContext(DbContextOptions options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public virtual DbSet<Course> Courses { get; set; }
+        public virtual DbSet<UsersCourses> UsersCourses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfiguration(new CoursesInitialData());
+            builder.ApplyConfiguration(new UsersCoursesConfig());
+            builder.ApplyConfiguration(new UsersInitialData());
+            builder.ApplyConfiguration(new UsersCoursesInitialData());
+            builder.ApplyConfiguration(new RoleInitialData());
+        }
+    }
+}
