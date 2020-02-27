821ef13324cdf77814ef4a482a07d26e63dc3963
Shafranskyi <shafranskyi_ak16@nuwm.edu.ua>
2020-02-27 23:20:27 +0200

    Changed Program and Startup. Changed appsettings.json

--- StudentsLearning/Program.cs
+++ StudentsLearning/Program.cs
@@ -1,24 +1,71 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using DataAccess.Entities;
+using System;
+using System.Linq;
 
 namespace StudentsLearning
 {
+    public static class SeedData
+    {
+        public static IWebHost SeedAdminUser(this IWebHost webHost)
+        {
+            using (var scope = webHost.Services.CreateScope())
+            {
+                try
+                {
+                    var context = scope.ServiceProvider.GetRequiredService<DbContext>();
+                    context.Database.EnsureCreated();
+                    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
+                    if (!userManager.Users.Any(u => u.Email == "admin@gmail.com"))
+                    {
+                        var user = new User
+                        {
+                            UserName = "Admin",
+                            Email = "admin@gmail.com",
+                            FirstName = "Admin",
+                            LastName = "Admin",
+                            Age = 38,
+                            EmailConfirmed = true,
+                            Gender = "Male",
+                            RegistrationDate = new DateTime(2017, 9, 18)
+                        };
+                        userManager.CreateAsync(user, "12345Admin")
+                        .Wait();
+                        userManager.AddToRoleAsync(user, "admin").Wait();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding user.");
+                }
+            }
+            return webHost;
+        }
+    }
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args).Build().SeedAdminUser().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        WebHost.CreateDefaultBuilder(args)
+        .ConfigureLogging((hostingContext, logging) =>
+        {
+            logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+            logging.AddConsole();
+            logging.AddDebug();
+            logging.AddEventSourceLogger();
+            logging.AddNLog();
+        })
+        .UseStartup<Startup>();
     }
 }
--- StudentsLearning/Startup.cs
+++ StudentsLearning/Startup.cs
@@ -1,13 +1,33 @@
+using AutoMapper;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using BusinessLogic.Interfaces;
+using BusinessLogic.Services;
+using BusinessLogic.Helpers;
+using DataAccess.Context;
+using DataAccess.Entities;
+using DataAccess.Interfaces;
+using DataAccess.Repository;
+using StudentsLearning.ViewModels.AuthViewModels;
+using StudentsLearning.ViewModels.AuthViewModels.Validation;
+using System.Reflection;
+using System.Text;
+using StudentsLearning.Helpers;
+using Hangfire;
 
 namespace StudentsLearning
 {
+
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -20,13 +40,72 @@ namespace StudentsLearning
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                .AddFluentValidation(fv =>
+                {
+                    fv.RegisterValidatorsFromAssemblyContaining<Startup>();
+                });
 
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/build";
             });
+
+            services.Configure<PhotoSettings>(Configuration.GetSection(nameof(PhotoSettings)));
+            services.AddScoped<DbContext, StudentsLearningDbContext>();
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            services.AddScoped<IAuthService, AuthService>();
+            services.AddScoped<IStudentService, StudentService>();
+            services.AddScoped<ICourseService, CourseService>();
+            services.AddScoped<IJwtFactory, JwtFactory>();
+            services.AddTransient<IValidator<LoginViewModel>, LoginValidation>();
+            services.AddSingleton<IEmailSender, EmailSender>();
+            services.AddScoped<IProfileService, ProfileService>();
+            services.AddTransient<IBackgroundEmailSender, BackgroundEmailSender>();
+
+            services.AddAutoMapper(new Assembly[]
+            {
+                typeof(Mapping.MappingProfile).GetTypeInfo().Assembly,
+                typeof(BusinessLogic.Mapping.MappingProfile).GetTypeInfo().Assembly
+            });
+
+            services.Configure<EmailSettings>(Configuration.GetSection("EmailSettings"));
+
+            services.AddDbContext<StudentsLearningDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("Default"),
+                m => m.MigrationsAssembly("StudentsLearning")));
+
+            services.AddDefaultIdentity<User>()
+                .AddRoles<IdentityRole<int>>()
+                .AddEntityFrameworkStores<StudentsLearningDbContext>();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 6;
+            });
+
+            services.AddAuthorization();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII
+                            .GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
+
+            services.AddHangfire(config =>
+                config.UseSqlServerStorage(Configuration.GetConnectionString("Default")));
+            services.AddHangfireServer();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,9 +122,12 @@ namespace StudentsLearning
                 app.UseHsts();
             }
 
+            app.UseHangfireServer();
+            app.UseHangfireDashboard();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
+            app.UseAuthentication();
 
             app.UseMvc(routes =>
             {
